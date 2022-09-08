467896a837d641a3425993ff4fa29131f6a33cb4
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-08 13:31:12 -0700

    4 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
 #!/bin/bash
-printf -s puts
+#include <stdio.h>
+int main() { // printf() displaying the string inside quoation 
+	printf("Programming is like building a multilingual puzzle);
+	return O;
+           }
