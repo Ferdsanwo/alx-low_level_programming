a19d185461cd491fe908af171afd2ebbd8800e26
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-09 11:34:18 -0700

    6 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*
-*Return: Always 0
-*/
+ *main - Enrty point
+ *
+ *Return: Always 0
+ */
 int main(void)
 {
 	printf(" Size of char: %zu byte(s)\n", sizeof(char));
