9734c5bcbb25a5dc44f6239c83c0cb75cbfcc9d9
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-09 09:10:47 -0700

    4 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 
-/**
-*main - Entry point
-*
-*Return: Always 0
-*/
-int main (void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	return(0);
 }
