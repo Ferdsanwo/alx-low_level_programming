c3decbf4c8b13e2f2c2db240dbac68bfd46cca96
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-12 18:09:04 -0700

    03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,19 +11,31 @@
 
 
 int main(void)
+
+
+
 {
+
 		char letter;
+
+
 
 			for (letter = 'a'; letter <= 'z'; letter++)
 
-				putchar(letter);
-
-			for (letter = 'A'; letter <= 'Z'; letter++)
-
-				putchar(letter);
-
-				putchar('\n');
+						putchar(letter);
 
 
-			return (0);
+
+				for (letter = 'A'; letter <= 'Z'; letter++)
+
+							putchar(letter);
+
+
+
+					putchar('\n');
+
+
+
+						return (0);
+
 }
