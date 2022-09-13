850f1e6cc91360ede6f09a6b1dac7af63c2f22e7
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-12 17:16:23 -0700

    09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+
+{
+
+		int i = 48;
+
+
+			while (i <= 57)
+
+					{
+
+								putchar(i);
+
+										if (i != 57)
+
+													{
+
+																	putchar(',');
+
+																				putchar(' ');
+
+																						}
+
+												i++;
+
+													}
+
+				putchar('\n');
+
+					return (0);
+
+}
