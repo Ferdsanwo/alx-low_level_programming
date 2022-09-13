82c372408975f6a38301784cbe78d2e3c1b185da
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-12 17:06:08 -0700

    07

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+
+{
+
+		char ch = 'z';
+
+			int i = 26;
+
+
+				while (i > 0)
+
+						{
+
+									putchar(ch);
+
+											ch--;
+
+													i--;
+
+														}
+
+					putchar('\n');
+
+						return (0);
+
+}
