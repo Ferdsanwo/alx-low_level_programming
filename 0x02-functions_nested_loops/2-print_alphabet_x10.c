f02bf27e6cd48112c5ebfccc39bd23fe00b6a841
Ademola sanwo <ademolaferdinarnd@gmail.com>
2022-09-26 17:19:59 -0700

    deleted files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
-
-
 /**
  *print_alphabet_x10 - print alphabet
  *
  *Return: Always 0.
  */
-
-
 
 void print_alphabet_x10(void)
 
@@ -18,34 +14,23 @@ void print_alphabet_x10(void)
 
 			int count;
 
-
-
-
-
 				count = 0;
 
-					while (count < 10)
+				while (count < 10)
+
+				{
+
+					for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 
 							{
 
-										for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+							_putchar(alphabet);
+												}
 
-													{
+								count++;
 
-																	_putchar(alphabet);
+								_putchar('\n');
 
-																			}
-
-
-
-												count++;
-
-														_putchar('\n');
-
-															}
-
-
-
-
+											}
 
 }
